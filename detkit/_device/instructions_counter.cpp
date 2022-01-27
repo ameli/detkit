26719085d1e1d2e7d65df051895145993e277ee1
Siavash Ameli <sia.sunrise@gmail.com>
2022-01-26 19:31:38 -0800

    pid_t exclusion

--- detkit/_device/instructions_counter.cpp
+++ detkit/_device/instructions_counter.cpp
@@ -30,20 +30,20 @@
 // perf event open
 // ===============
 
-static long perf_event_open(
-        struct perf_event_attr* hw_event,
-        pid_t pid,
-        int cpu,
-        int group_fd,
-        unsigned long flags)
-{
-    #if __linux__
+#if __linux__
+    static long perf_event_open(
+            struct perf_event_attr* hw_event,
+            pid_t pid,
+            int cpu,
+            int group_fd,
+            unsigned long flags)
+    {
         int ret;
         ret = syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd,
                       flags);
         return ret;
-    #endif
-}
+    }
+#endif
 
 
 // ===========
