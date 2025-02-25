0c5c03477d55a7800293d76dc5f41c5a687a98c8
Siavash Ameli <sia.sunrise@gmail.com>
2025-02-24 16:24:01 -0800

    test runner threads 5

--- tests/test_memdet.py
+++ tests/test_memdet.py
@@ -188,11 +188,11 @@ def test_memdet():
     # # Test various parallel io
     if sys.version_info[:2] == (3, 12):
         # In Python 3.12, dask takes forever.
-        # parallel_ios = [None, 'multiproc', 'tensorstore']
-        parallel_ios = ['multiproc']
+        parallel_ios = [None, 'multiproc', 'tensorstore']
+        # parallel_ios = ['multiproc']
     else:
-        # parallel_ios = [None, 'multiproc', 'dask', 'tensorstore']
-        parallel_ios = ['multiproc']
+        parallel_ios = [None, 'multiproc', 'dask', 'tensorstore']
+        # parallel_ios = ['multiproc']
 
     _test(A, assumes=['gen'], dtypes=['float64'], triangles=[None],
           parallel_ios=parallel_ios, num_blocks=[4], max_mems=[float('inf')])
